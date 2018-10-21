7e39f8e0286be6b451fbd16e12f04ff86ea80224
Bob Benson <bobbethelbenson@gmail.com>
2018-10-21 16:59:37 +0100

    script

---
+++ run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+    --region eu-west-1 \
+    --stack-name $STACK_NAME \
+    --template-body file://service.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+    ParameterKey=DockerImage,ParameterValue=$ADD_SERVER_ADDRESS_HERE/exampl-webapp:$(git rev-parse HEAD) \
+    ParameterKey=VPC,ParameterValue=$ADD_VPC_HERE
+    ParameterKey=Cluster,ParameterValue=default \
+    ParameterKey=Listener,ParameterValue=$ADD_ARN_HERE
